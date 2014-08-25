135b93d4e30592785c9aeb58184f37f506762d7c
LisaStoz <lisa@lisastoz.com>
2014-08-25 20:26:10 +0100

    slideshow functionality

--- slideshow/slideshow.js
+++ slideshow/slideshow.js
@@ -1,1 +1,53 @@
 var filmroll = document.getElementById('the-film-roll');
+
+var scrolling = false;
+
+function scroll(start, end) {
+  var current = start;
+
+  var move = function () {
+    filmroll.style.left = current + 'px';
+
+    if (current == end) {
+      clearInterval(loop);
+      scrolling = false;
+    }
+
+    if (end > start) {
+      current = Math.min(current + 20, end);
+    } else {
+      current = Math.max(current - 20, end);
+    }
+  }
+
+  var loop = setInterval(move, 50);
+  scrolling = true;
+}
+
+function handleEvent(e) {
+  if (!scrolling) {
+    backAndForth(e.keyCode);
+  }
+}
+
+document.addEventListener('keydown', handleEvent);
+
+function handleEvent(e) {
+  if (!scrolling) {
+    backAndForth(e.keyCode);
+  }
+}
+
+var current = 0;
+
+function backAndForth(keyCode) {
+  if (keyCode == 37) {
+    scroll(current, current - 612);
+    current = current - 612;
+  }
+
+  if (keyCode == 39) {
+    scroll(current, current + 612);
+    current = current + 612;
+  }
+}
